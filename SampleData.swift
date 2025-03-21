1432534db7064f701e1adfcbbdab2359dd09987e
Ceren Isci <101287638@georgebrown.ca>
2025-03-21 14:53:57 -0400

    commit changes

--- SampleData.swift
+++ SampleData.swift
@@ -2,7 +2,7 @@ import CoreData
 
 func addSampleProducts(context: NSManagedObjectContext) {
     let products = [
-        ("1", "Laptop", "High-performance laptop", 1300.0, "TechComp"),
+        ("1", "Laptop", "Most rated laptop", 1300.0, "TechComp"),
         ("2", "Smartphone", "Latest model smartphone", 900.0, "Mobile Phone Inc"),
         ("3", "Tablet", "10-inch screen tablet", 500.0, "Tab World")
     ]
